90d18c64a90d52f0f0aaa2ed729ffceb1ce3871b
antD <foshizzle97@gmail.com>
2019-09-21 15:19:09 -0400

    fixed number key binds

--- home/.config/awesome/rc_colemak-dh.lua
+++ home/.config/awesome/rc_colemak-dh.lua
@@ -478,7 +478,12 @@ clientkeys = gears.table.join(
 -- Bind all key numbers to tags.
 -- Be careful: we use keycodes to make it work on any keyboard layout.
 -- This should map on the top row of your keyboard, usually 1 to 9.
-for i = 1, 5 do
+num_displays = 0
+for _ in pairs(xrandr.outputs()) do num_displays = num_displays + 1 end
+
+if num_displays == 1 then n_tags = 10 else n_tags = 5 end
+
+for i = 1, n_tags do
 	globalkeys = gears.table.join(globalkeys,
 		-- View tag only.
 		awful.key({ modkey }, "#" .. i + 9,
@@ -490,16 +495,6 @@ for i = 1, 5 do
 						end
 				  end,
 				  {description = "View tag #"..i, group = "Tag"}),
-		-- Toggle tag display.
-		awful.key({ modkey, "Control" }, "#" .. i + 9,
-				  function()
-					  local screen = awful.screen.focused()
-					  local tag = screen.tags[i]
-					  if tag then
-						 awful.tag.viewtoggle(tag)
-					  end
-				  end,
-				  {description = "Toggle tag #" .. i, group = "Tag"}),
 		-- Move client to tag.
 		awful.key({ modkey, "Shift" }, "#" .. i + 9,
 				  function()
@@ -510,18 +505,7 @@ for i = 1, 5 do
 						  end
 					 end
 				  end,
-				  {description = "Move focused client to tag #"..i, group = "Tag"}),
-		-- Toggle tag on focused client.
-		awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
-				  function()
-					  if client.focus then
-						  local tag = client.focus.screen.tags[i]
-						  if tag then
-							  client.focus:toggle_tag(tag)
-						  end
-					  end
-				  end,
-				  {description = "Toggle focused client on tag #" .. i, group = "Tag"})
+				  {description = "Move focused client to tag #"..i, group = "Tag"})
 	)
 end
 
@@ -536,6 +520,7 @@ root.keys(globalkeys)
 
 -- Rules
 --------------------------------------------------------------------------------
+num_displays = 0
 for _ in pairs(xrandr.outputs()) do num_displays = num_displays + 1 end
 if num_displays == 1 then discord_screen = 1
 else discord_screen = 2 end
